7c9304b8713425ff8492e442b1223b561c4fcc68
flvffywvffy <marcus@marcusleonasharvey.com>
2025-03-23 14:46:03 +0000

    update layout & page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import "~/styles/globals.css";
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
+import { Navbar } from "~/components/navbar";
+import { auth, signIn } from "~/server/auth";
+import { Button } from "~/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,12 +17,33 @@ const geist = Geist({
   variable: "--font-geist-sans",
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const session = await auth();
+
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
+      <body className="px-2 py-2 md:mx-auto md:max-w-2xl">
+        <Navbar />
+        <main>
+          {session ? (
+            <>{children}</>
+          ) : (
+            <main className="flex flex-col items-center justify-center gap-2 py-40">
+              <h1 className="text-4xl font-semibold">FluffyPresence</h1>
+              <Button
+                onClick={async () => {
+                  "use server";
+                  await signIn("discord");
+                }}
+              >
+                Sign in with Discord
+              </Button>
+            </main>
+          )}
+        </main>
+      </body>
     </html>
   );
 }
